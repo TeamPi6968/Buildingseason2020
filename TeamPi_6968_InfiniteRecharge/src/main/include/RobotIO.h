bf8089699984e93e31d1cd87b9c29ba6b4e5cfac
jesperweijnen <49162811+jesperweijnen@users.noreply.github.com>
2020-02-06 13:18:52 +0100

    RobotIO.h recreated and fixed

---
+++ TeamPi_6968_InfiniteRecharge/src/main/include/RobotIO.h
@@ -0,0 +1,27 @@
+#pragma once
+
+//In this class, all defines and constants can be found.
+
+//CANID's
+    //Drivetrain Motorcontrollers:
+    int drivetrainLeftFront = 1;
+    int drivetrainLeftBack = 2;
+    int drivetrainRightBack = 3;
+    int drivetrainRightFront = 4;
+    //---------------------------
+    
+    //Intake Motorcontrollers:
+
+    //---------------------------
+
+    //Storage Motorcontrollers:
+
+    //---------------------------
+
+    //Outtake Motorcontrollers:
+
+    //---------------------------
+
+    //Control Panel Motorcontrollers:
+
+    //---------------------------
