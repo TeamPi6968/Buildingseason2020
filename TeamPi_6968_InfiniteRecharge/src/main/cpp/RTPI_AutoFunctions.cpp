1a374ff48703a43495d088a1d0f0d2a5b587b303
jesperweijnen <49162811+jesperweijnen@users.noreply.github.com>
2020-02-11 15:05:43 +0100

    Added AutoFunctions class and stopAutoFunction for safety stops during auto functions

---
+++ TeamPi_6968_InfiniteRecharge/src/main/cpp/RTPI_AutoFunctions.cpp
@@ -0,0 +1,19 @@
+#include "RTPI_AutoFunctions.h"
+
+RTPI_AutoFunctions::RTPI_AutoFunctions(RobotIO *_robotIO, RTPI_ControllerInput *_input, RTPI_Drivetrain *_drivetrain, RTPI_Intake *_intake, RTPI_Storage *_storage, RTPI_Outtake *_outtake) {
+  robotIO = _robotIO;
+  input = _input;
+  drivetrain = _drivetrain;
+  intake = _intake;
+  storage = _storage;
+  outtake = _outtake;
+}
+
+bool RTPI_AutoFunctions::stopAutoFunction() {
+  if(input->navigator->GetRawButton(7) || input->driver->GetRawButton(7)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
---
+++ TeamPi_6968_InfiniteRecharge/src/main/include/RTPI_AutoFunctions.h
@@ -0,0 +1,28 @@
+/*
+-   Created By: Jesper Weijnen
+-   Date: 11/02/2020
+-   Time: 13:57
+-   Class: RTPI_AutoFunctions
+*/
+
+#pragma once
+
+#include "RobotIO.h"
+#include "RTPI_ControllerInput.h"
+#include "RTPI_Drivetrain.h"
+#include "RTPI_Intake.h"
+#include "RTPI_Storage.h"
+#include "RTPI_Outtake.h"
+
+class RTPI_AutoFunctions {
+ private:
+  RobotIO *robotIO;
+  RTPI_ControllerInput *input;
+  RTPI_Drivetrain *drivetrain;
+  RTPI_Intake *intake;
+  RTPI_Storage *storage;
+  RTPI_Outtake *outtake;
+  bool stopAutoFunction(); //Safety Button
+ public:
+  RTPI_AutoFunctions(RobotIO *_robotIO, RTPI_ControllerInput *_input, RTPI_Drivetrain *_drivetrain, RTPI_Intake *_intake, RTPI_Storage *_storage, RTPI_Outtake *_outtake);
+};
