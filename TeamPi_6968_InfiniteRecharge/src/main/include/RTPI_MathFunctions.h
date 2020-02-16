33b114e4651e1d97396c39bec88b6164d858fde9
jesperweijnen <49162811+jesperweijnen@users.noreply.github.com>
2020-02-16 01:23:59 +0100

    Added TransmissionTransferReversed to mathFunctions class and updated Storage class

--- TeamPi_6968_InfiniteRecharge/src/main/include/RTPI_MathFunctions.h
+++ TeamPi_6968_InfiniteRecharge/src/main/include/RTPI_MathFunctions.h
@@ -12,5 +12,6 @@ class RTPI_MathFunctions {
 
  public:
   double MapDouble(double input, double minInput, double maxInput, double minOutput, double maxOutput); //Maps a number to another range
-  double TransmissionTransfer(double input, double transmissionInput, double transmissionOutput); //Calculates the output of a gearbox. Insert the rotation input and give the transmissionInput and Output (transIn:TransOut)
+  double TransmissionTransfer(double input, double transmissionInput, double transmissionOutput); //Calculates the output of a gearbox. Insert the rotation input and give the transmissionInput and Output (transmissionInput:transmissionOutpu)
+  double TransmissionTransferReversed(double output, double transmissionInput, double transmissionOutput); //Calculates the input of a gearbox. Insert the rotation output and give the transmissionInput and Output (transmissionInput:transmissionOutput)
 };
--- TeamPi_6968_InfiniteRecharge/src/main/include/RTPI_Storage.h
+++ TeamPi_6968_InfiniteRecharge/src/main/include/RTPI_Storage.h
@@ -8,11 +8,13 @@
 #pragma once
 
 #include "RTPI_SparkMax.h"
+#include "RTPI_MathFunctions.h"
 
 class RTPI_Storage {
  private:
   RTPI_SparkMax *sparkRevolver;
   RTPI_SparkMax *sparkLoader;
+  RTPI_MathFunctions *mathFunctions;
   double currentRevolverPosition;
  public:
   RTPI_Storage(RTPI_SparkMax *storageRevolver, RTPI_SparkMax *storageLoader);
